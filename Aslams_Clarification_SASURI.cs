b023e13e2b23d3b1ad4966a2ebf7c6b08fa9d84a
Gaurav8032 <Gaurav8032@users.noreply.github.com>
2019-02-15 00:06:30 +0900

    Added small program for clarification to Aslam
    added SAS URI genereation program. Which depicts how to generate SAS uri using RBAC. Following code restricts access to blob storage (my personal azure account blob storage) for only list and append rights. Any user who generates the sas uri gets only 2 access for following blob storage. Happy Coding !!!

---
+++ Aslams_Clarification_SASURI.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace ConsoleApp2
+{    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parse the connection string and return a reference to the storage account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            //Create the blob client object.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            //Get a reference to a container to use for the sample code, and create it if it does not exist.
+            CloudBlobContainer container = blobClient.GetContainerReference("sascontainer");
+            container.CreateIfNotExists();
+
+             //Generate a SAS URI for the container, without a stored access policy.
+            Console.WriteLine("Container SAS URI: " + GetContainerSasUri(container));
+            Console.WriteLine();
+
+            //Require user input before closing the console window.
+            Console.ReadLine();
+        }
+        static string GetContainerSasUri(CloudBlobContainer container)
+        {
+            //Set the expiry time and permissions for the container.
+            //In this case no start time is specified, so the shared access signature becomes valid immediately.
+            SharedAccessBlobPolicy sasConstraints = new SharedAccessBlobPolicy();
+            sasConstraints.SharedAccessExpiryTime = DateTimeOffset.UtcNow.AddHours(24);
+
+            //Below here you create your RBAC scene :: For Aslam to understand
+            sasConstraints.Permissions = SharedAccessBlobPermissions.List | SharedAccessBlobPermissions.Write;
+
+            //Generate the shared access signature on the container, setting the constraints directly on the signature.
+            string sasContainerToken = container.GetSharedAccessSignature(sasConstraints);
+
+            //Return the URI string for the container, including the SAS token.
+            return container.Uri + sasContainerToken;
+        }
+    }
+}
